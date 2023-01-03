29b2cb9879214ee11215bdfefb1e3b4e0759a048
JiangTaoHou <925147954@qq.com>
2023-01-03 22:19:12 +0800

    调试通过

---
+++ imooc-security-demo/src/main/java/com/imooc/comtroller/UserController.java
@@ -0,0 +1,21 @@
+package com.imooc.comtroller;
+
+import com.imooc.dto.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+    @GetMapping("/user")
+    @ResponseBody
+    public List<User> query(@RequestParam("username")String nickname){
+        System.out.println(nickname);
+        List<User> users = new ArrayList<>();
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+        return users;
+    }
+}
