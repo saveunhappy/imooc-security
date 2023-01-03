5a14a7bbc71d10981df4ece4e651fffa3493f0b9
JiangTaoHou <925147954@qq.com>
2023-01-03 21:34:28 +0800

    调试通过

---
+++ imooc-security-demo/src/main/java/com/imooc/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run( DemoApplication.class, args );
+    }
+}
