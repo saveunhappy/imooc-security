38642c5595a6ee77d43476b4b10ebc3996a43ef1
JiangTaoHou <925147954@qq.com>
2023-01-05 01:28:14 +0800

    DeferredResult,多线程使用，然后可以在设置拦截器，在拦截器中设置其他的东西，比如线程池，超时时间之类的。

--- imooc-security-demo/src/main/java/com/imooc/comtroller/AsyncController.java
+++ imooc-security-demo/src/main/java/com/imooc/comtroller/AsyncController.java
@@ -1,30 +1,44 @@
 package com.imooc.comtroller;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
+import javax.annotation.Resource;
 import java.util.concurrent.Callable;
 
 
 @RestController
 public class AsyncController {
+    @Resource
+    private MockQueue mockQueue;
+    @Resource
+    private DeferredResultHolder holder;
     private Logger logger = LoggerFactory.getLogger(getClass());
     @RequestMapping("/order")
-    public Callable<String> order() throws InterruptedException {
+    public DeferredResult<String> order() throws InterruptedException {
         logger.info("主线程开始");
-        Callable<String> callable = new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                logger.info("副线程开始");
-                Thread.sleep(1000);
-                logger.info("副线程结束");
+        String orderNumber = RandomStringUtils.randomNumeric(8);
+        mockQueue.setPlaceOrder(orderNumber);
+        DeferredResult<String> result = new DeferredResult<>();
+        holder.getMap().put(orderNumber,result);
+//        Callable<String> callable = new Callable<String>() {
+//            @Override
+//            public String call() throws Exception {
+//                logger.info("副线程开始");
+//                Thread.sleep(1000);
+//                logger.info("副线程结束");
+//
+//                return "success";
+//            }
+//        };
 
-                return "success";
-            }
-        };
         logger.info("主线程返回");
-        return callable;
+        return result;
     }
 }
