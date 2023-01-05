984a57b3676e270df015933eb0cc68e1ec524cdf
JiangTaoHou <925147954@qq.com>
2023-01-05 21:34:48 +0800

    自定义用户认证逻辑

--- imooc-security-browser/src/main/java/com/imooc/security/browser/MyUserDetailService.java
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/MyUserDetailService.java
@@ -2,20 +2,29 @@ package com.imooc.security.browser;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 @Component
 public class MyUserDetailService implements UserDetailsService {
     private static final Logger logger = LoggerFactory.getLogger(MyUserDetailService.class);
+    @Resource
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         //根据用户名查询用户信息
         logger.info("登录用户名" + username);
-        return new User(username,"123456", AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+        return new User(username,passwordEncoder.encode("123456"),
+                true,true,true,true,
+                AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
     }
 }
