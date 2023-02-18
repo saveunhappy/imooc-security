6d0c953e6053f3d43e1807d9c29b820a70a15d89
JiangTaoHou <925147954@qq.com>
2023-02-18 20:45:56 +0800

    1.AntPathMatcher进行类似于正则表达式的匹配 2.InitializingBean进行初始化

--- imooc-security-core/src/main/java/com/imooc/security/core/validate/code/ValidateCodeFilter.java
+++ imooc-security-core/src/main/java/com/imooc/security/core/validate/code/ValidateCodeFilter.java
@@ -1,10 +1,13 @@
 package com.imooc.security.core.validate.code;
 
+import com.imooc.security.core.properties.SecurityProperties;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.social.connect.web.HttpSessionSessionStrategy;
 import org.springframework.social.connect.web.SessionStrategy;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -15,18 +18,44 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
-public class ValidateCodeFilter extends OncePerRequestFilter {
+public class ValidateCodeFilter extends OncePerRequestFilter implements InitializingBean {
     //执行一次的filter,这个是在UserNamePassword过滤器之前执行的，自定义的。
     //如果是自定义的，我们想拦截的，那么就可以拦截到了，否则，走到else里面，直接放行。
     @Autowired
     private AuthenticationFailureHandler authenticationFailureHandler;
 
     private SessionStrategy sessionStrategy = new HttpSessionSessionStrategy();
-    @Override
+
+    private Set<String> urls = new HashSet<>();
+
+    private SecurityProperties securityProperties;
+
+    private AntPathMatcher pathMatcher = new AntPathMatcher();
+
+	@Override
+	public void afterPropertiesSet() throws ServletException {
+		super.afterPropertiesSet();
+		String[] configUrls = StringUtils.splitByWholeSeparatorPreserveAllTokens(securityProperties.getCode().getImage().getUrl(),",");
+		urls.addAll(Arrays.asList(configUrls));
+		urls.add("/authentication/form");
+	}
+
+	@Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (StringUtils.equals("/authentication/form", request.getRequestURI())
-                && StringUtils.endsWithIgnoreCase(request.getMethod(), "post")) {
+       boolean action = false;
+       for(String url: urls){
+       	if(pathMatcher.match(url,request.getRequestURI())){
+       		action = true;
+		}
+	   }
+//        if (StringUtils.equals("/authentication/form", request.getRequestURI())
+//                && StringUtils.endsWithIgnoreCase(request.getMethod(), "post"))
+        if (action)
+        {
             try {
                 validate(new ServletWebRequest(request));
             } catch (ValidateCodeException e) {
@@ -42,7 +71,8 @@ public class ValidateCodeFilter extends OncePerRequestFilter {
 	private void validate(ServletWebRequest request) throws ServletRequestBindingException {
 		ImageCode codeInSession = (ImageCode)sessionStrategy.getAttribute(request,
 				ValidateCodeController.SESSION_KEY);
-		String codeInRequest = ServletRequestUtils.getStringParameter(request.getRequest(), "imageCode");
+		String codeInRequest = ServletRequestUtils.getStringParameter
+				(request.getRequest(), "imageCode");
 
 		if (StringUtils.isBlank(codeInRequest)) {
 			throw new ValidateCodeException("验证码的值不能为空");
@@ -70,5 +100,21 @@ public class ValidateCodeFilter extends OncePerRequestFilter {
 
 	public void setAuthenticationFailureHandler(AuthenticationFailureHandler authenticationFailureHandler) {
 		this.authenticationFailureHandler = authenticationFailureHandler;
+	}
+
+	public Set<String> getUrls() {
+		return urls;
+	}
+
+	public void setUrls(Set<String> urls) {
+		this.urls = urls;
+	}
+
+	public SecurityProperties getSecurityProperties() {
+		return securityProperties;
+	}
+
+	public void setSecurityProperties(SecurityProperties securityProperties) {
+		this.securityProperties = securityProperties;
 	}
 }
