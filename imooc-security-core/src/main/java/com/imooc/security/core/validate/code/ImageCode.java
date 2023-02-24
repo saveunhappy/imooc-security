375d634c5c1bdb768ebbf0cc57aec1e0bc5d9a11
JiangTaoHou <925147954@qq.com>
2023-02-24 15:35:23 +0800

    提取出父类之后，子类原来的属性一定要删除，否则就出问题了

--- imooc-security-core/src/main/java/com/imooc/security/core/validate/code/ImageCode.java
+++ imooc-security-core/src/main/java/com/imooc/security/core/validate/code/ImageCode.java
@@ -6,10 +6,6 @@ import java.time.LocalDateTime;
 public class ImageCode extends ValidateCode {
     private BufferedImage image;
 
-    private String code;
-
-    private LocalDateTime expireTime;
-
     public ImageCode(BufferedImage image, String code, LocalDateTime expireTime) {
         super(code,expireTime);
         this.image = image;
@@ -28,23 +24,4 @@ public class ImageCode extends ValidateCode {
         this.image = image;
     }
 
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public LocalDateTime getExpireTime() {
-        return expireTime;
-    }
-
-    public void setExpireTime(LocalDateTime expireTime) {
-        this.expireTime = expireTime;
-    }
-
-    public boolean isExpired() {
-        return LocalDateTime.now().isAfter(expireTime);
-    }
 }
