48d5f2b2a66202e292ecf4ef6fabd40ea510e98c
JiangTaoHou <925147954@qq.com>
2023-03-13 21:35:41 +0800

    微信登录完成

--- imooc-security-core/src/main/java/com/imooc/security/core/properties/SocialProperties.java
+++ imooc-security-core/src/main/java/com/imooc/security/core/properties/SocialProperties.java
@@ -12,6 +12,8 @@ public class SocialProperties {
 
     private QQProperties qq = new QQProperties();
 
+    private WeixinProperties weixin = new WeixinProperties();
+
     public QQProperties getQq() {
         return qq;
     }
@@ -26,5 +28,13 @@ public class SocialProperties {
 
     public void setFilterProcessesUrl(String filterProcessesUrl) {
         this.filterProcessesUrl = filterProcessesUrl;
+    }
+
+    public WeixinProperties getWeixin() {
+        return weixin;
+    }
+
+    public void setWeixin(WeixinProperties weixin) {
+        this.weixin = weixin;
     }
 }
